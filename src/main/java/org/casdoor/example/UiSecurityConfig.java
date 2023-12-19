e17be3ba6da5973c939fdd4d11fa4e6eaad2195d
Lord_One <lord2323690039@163.com>
2023-12-19 21:21:29 +0800

    add myLogout function when you logout auto direct to /*/api/logout then wait for 2seconds direct to original login page I change the url from https://door/casdoor.com to my localhost

--- src/main/java/org/casdoor/example/UiSecurityConfig.java
+++ src/main/java/org/casdoor/example/UiSecurityConfig.java
@@ -3,6 +3,7 @@ package org.casdoor.example;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,7 +11,11 @@ public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/login**").permitAll().anyRequest().authenticated().and()
-                .oauth2Login();
-
+                .oauth2Login().and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutUrl("http://localhost:7001/api/logout")
+                .invalidateHttpSession(true)
+                .permitAll();
     }
 }
--- src/main/java/org/casdoor/example/controller/FooClientController.java
+++ src/main/java/org/casdoor/example/controller/FooClientController.java
@@ -1,24 +1,50 @@
 package org.casdoor.example.controller;
 
 import org.casdoor.example.model.FooModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class FooClientController {
     @GetMapping("/foos")
-    public String getFoos(Model model, Authentication authentication) {
+    public String getFoos(Model model, Authentication authentication,HttpSession session) {
         List<FooModel> foos = new ArrayList<>();
         foos.add(new FooModel(1L, "a"));
         foos.add(new FooModel(2L, "b"));
         foos.add(new FooModel(3L, "c"));
         foos.add(new FooModel(4L, authentication.getName()));
         model.addAttribute("foos", foos);
+        session.setAttribute("user",authentication.getName());
         return "foos";
     }
+
+    @Autowired
+    RestTemplate  restTemplate;
+    @GetMapping("/logout")
+    public String myLogout(Model model) {
+        String forObject = restTemplate.getForObject("http://localhost:7001/api/logout", String.class);
+        System.out.println(forObject);
+        model.addAttribute("logoutSuccess", true);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "index";
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
