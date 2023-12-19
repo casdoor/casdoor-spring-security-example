5ff54a20b97cd1887c55bd440d12d7dfee264043
Lord_One <lord2323690039@163.com>
2023-12-19 22:11:31 +0800

    login :add information into session logout :delete information from session

--- src/main/java/org/casdoor/example/controller/FooClientController.java
+++ src/main/java/org/casdoor/example/controller/FooClientController.java
@@ -4,6 +4,7 @@ import org.casdoor.example.model.FooModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,15 +24,17 @@ public class FooClientController {
         foos.add(new FooModel(3L, "c"));
         foos.add(new FooModel(4L, authentication.getName()));
         model.addAttribute("foos", foos);
-        session.setAttribute("user",authentication.getName());
+        DefaultOAuth2User user =(DefaultOAuth2User) authentication.getPrincipal();
+        session.setAttribute("user",user);
         return "foos";
     }
 
     @Autowired
     RestTemplate  restTemplate;
     @GetMapping("/logout")
-    public String myLogout(Model model) {
+    public String myLogout(Model model,HttpSession session) {
         String forObject = restTemplate.getForObject("http://localhost:7001/api/logout", String.class);
+        session.setAttribute("user",null);
         System.out.println(forObject);
         model.addAttribute("logoutSuccess", true);
         try {
