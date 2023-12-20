06ecea54554d9dbeadd666ea69701b0628c70c29
Lordfiber <69179042+Lordfiber@users.noreply.github.com>
2023-12-20 21:34:00 +0800

    feat: add logout button (#5)
    * add myLogout function
    when you logout
    auto direct to /*/api/logout
    then wait for 2seconds
    direct to original login page
    I change the url from https://door/casdoor.com to my localhost
    
    * login :add information into session
    logout :delete information from session
    
    * adjust code
    change casdoor url

--- src/main/java/org/casdoor/example/UiSecurityConfig.java
+++ src/main/java/org/casdoor/example/UiSecurityConfig.java
@@ -3,6 +3,7 @@ package org.casdoor.example;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,7 +11,10 @@ public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/login**").permitAll().anyRequest().authenticated().and()
-                .oauth2Login();
-
+                .oauth2Login().and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .permitAll();
     }
 }
--- src/main/java/org/casdoor/example/controller/FooClientController.java
+++ src/main/java/org/casdoor/example/controller/FooClientController.java
@@ -1,24 +1,51 @@
 package org.casdoor.example.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.casdoor.example.model.FooModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class FooClientController {
     @GetMapping("/foos")
-    public String getFoos(Model model, Authentication authentication) {
+    public String getFoos(Model model, Authentication authentication, HttpSession session) throws JsonProcessingException {
         List<FooModel> foos = new ArrayList<>();
         foos.add(new FooModel(1L, "a"));
         foos.add(new FooModel(2L, "b"));
         foos.add(new FooModel(3L, "c"));
         foos.add(new FooModel(4L, authentication.getName()));
         model.addAttribute("foos", foos);
+        //get detail user information
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        session.setAttribute("user", user);
         return "foos";
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/logout")
+    public String myLogout(Model model, HttpSession session) {
+        String forObject = restTemplate.getForObject("https://door.casdoor.com/api/logout", String.class);
+        session.setAttribute("user", null);
+        System.out.println(forObject);
+        model.addAttribute("logoutSuccess", true);
+        return "index";
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
